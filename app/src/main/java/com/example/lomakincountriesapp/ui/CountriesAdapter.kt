7996737f7696dccde6382e21eb793788ce9244fa
Алексей <lomakinlexa@yandex.ru>
2023-11-25 15:49:02 +0400

    Change ListView to RecyclerView. Change the adapter for the View. Add country flags loading via Glide to the adapter

--- app/src/main/java/com/example/lomakincountriesapp/ui/CountriesAdapter.kt
+++ app/src/main/java/com/example/lomakincountriesapp/ui/CountriesAdapter.kt
@@ -4,33 +4,52 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
 import com.example.lomakincountriesapp.R
 
-class CountriesAdapter(context: Context,  private val items: List<String?>): ArrayAdapter<String?>(context, R.layout.country_fragment, items) {
+class CountriesAdapter(private val itemsNames: List<String?>,
+                       private val itemsFlags: List<String?>,
+                       private var context: Context
+):
+    RecyclerView.Adapter<CountriesAdapter.CountriesViewHolder>() {
 
+class CountriesViewHolder(view: View) : ViewHolder(view) {
+    val textView: TextView
+    val flag: ImageView
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val  convertView = LayoutInflater
-            .from(context).
-            inflate(R.layout.country_fragment,
-                parent,
-                false)
-
-        val textView = convertView.findViewById<TextView>(R.id.text_view)
-        val imageView = convertView.findViewById<ImageView>(R.id.flag)
-        textView.text = items[position]
-
-        if (position % 2 == 0)
-            imageView.setImageResource(R.drawable.flag)
-        else
-            imageView.setImageResource(R.drawable.pirate_flag)
-
-
-
-        return convertView
+    init {
+        textView = view.findViewById(R.id.text_view)
+        flag = view.findViewById(R.id.flag)
     }
+}
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup,
+                                    viewType: Int): CountriesViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.country_fragment,viewGroup,false)
+
+        return CountriesViewHolder(view)
+    }
+    
+    override fun getItemCount(): Int = itemsNames.size
+
+    override fun onBindViewHolder(viewHolder: CountriesViewHolder, position: Int) {
+        val country = itemsNames[position]
+        val flag = itemsFlags[position]
+
+        with(viewHolder.bindingAdapter) {
+            viewHolder.textView.text = country
+                Glide.with(context)
+                    .load(flag)
+                    .into(viewHolder.flag)
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/lomakincountriesapp/ui/CountriesListFragment.kt
+++ app/src/main/java/com/example/lomakincountriesapp/ui/CountriesListFragment.kt
@@ -3,6 +3,7 @@ package com.example.lomakincountriesapp.ui
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.lomakincountriesapp.R
 import com.example.lomakincountriesapp.data.Country
@@ -22,28 +23,39 @@ class CountriesListFragment: Fragment(R.layout.countries_list_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?, ) {
         super.onViewCreated(view, savedInstanceState)
+
         val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory
                 .create())
             .baseUrl(baseUrl)
             .build()
+
         val retrofitService: CountriesService = retrofit
             .create(CountriesService::class.java)
-
+        val manager = LinearLayoutManager(requireContext())
         CoroutineScope(Dispatchers.IO).launch {
             val countriesList = retrofitService
                 .getAllCountries()
+                .sortedBy { it.name?.common }
 
             val countriesNameList = countriesList
                 .map { country: Country ->
                 country.name?.common
             }
 
+
+            val countryFlagsList = countriesList.map{
+                country: Country ->
+                country.flags?.png
+            }
+
             withContext(Dispatchers.Main){
                 val adapter = CountriesAdapter(
-                    requireContext(),
-                    countriesNameList
+                    countriesNameList,
+                    countryFlagsList,
+                    requireContext()
                 )
+                binding.countriesList.layoutManager = manager
                 binding.countriesList.adapter = adapter
             }
         }
