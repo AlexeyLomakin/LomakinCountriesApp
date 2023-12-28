9621cb8a0dab26750520ce581e31f7ebb879226c
Алексей <lomakinlexa@yandex.ru>
2023-12-28 12:33:41 +0400

    Add MVVM and Dagger to the project

--- app/src/main/java/com/example/lomakincountriesapp/ui/viewmodels/ArtsViewModel.kt
+++ app/src/main/java/com/example/lomakincountriesapp/ui/viewmodels/ArtsViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.lomakincountriesapp.ui.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -12,7 +13,8 @@ import kotlinx.coroutines.withContext
 
 class ArtsViewModel(private val artsService: ArtsService) : ViewModel() {
 
-    val artsData = MutableLiveData<List<Arts>>()
+    private val _artsData = MutableLiveData<List<Arts>>()
+    val artsData: LiveData<List<Arts>> = _artsData
     private var currentPage = 1
 
     init {
@@ -29,7 +31,7 @@ class ArtsViewModel(private val artsService: ArtsService) : ViewModel() {
             val arts = withContext(Dispatchers.IO) {
                 artsService.getArtByPage(page)
             }
-            artsData.value = arts.data
+            _artsData.value = arts.data
         }
     }
 }
