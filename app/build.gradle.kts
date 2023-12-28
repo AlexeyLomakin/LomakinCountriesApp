9b13c7ef6257fcaea587d2e66b0acf78edf73343
Алексей <lomakinlexa@yandex.ru>
2023-12-28 12:11:17 +0400

    Add MVVM and Dagger to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.9.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -54,14 +54,14 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.10.1")
 
     //fragment
-    implementation ("androidx.fragment:fragment-ktx:1.3.0")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
 
     //retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     //coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 
     //ViewBindingPropertyDelegate
     implementation ("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.5.9")
